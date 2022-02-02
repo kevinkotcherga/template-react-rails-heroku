ce04a96c9aa4f73349ae712c153b986fcc9d8ba2
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-02 11:13:53 +0100

    ✨feat(seed): ajout de seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+u = User.create(username: "elmo", password: "elmo123")
+
+Note.create(title: "Ruby is amazing", content: "I learned how to build an ORM and use a database with a mix of SQL and Ruby", user: u)
+Note.create(title: "React is a great and dynamic front end technology", content: "Now I know how to build a full stack application using Rails for backend and React for the frontend", user: u)
+Note.create(title: "Deploying today", content: "Today I am learning how to deploy an application to the web", user: u)
