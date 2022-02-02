d355fdfef4b634b12dd4a7b9e78b88bd765fea4f
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-02 11:57:44 +0100

    ✨feat(react): connecter l'api rails avec react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from 'react'
 
 function App() {
+
+  useEffect(() => {
+    fetch('api/notes')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
